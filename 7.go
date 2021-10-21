0fe71383fffc1d78cfb69aa40ce0e92593c6b122
Prima Tondy Arya Kurniawan <primakurniawan666@gmail.com>
2021-10-21 15:20:46 +0700

    no 8

---
+++ 7.go
@@ -0,0 +1,25 @@
+
+package main
+ 
+
+
+ 
+
+func playWithAsterik(n int) {
+
+   // your code here
+   for i := 1; i <= n; i++ {
+	for (space = 1; space <= n - i; ++space) {
+	   printf("  ");
+	}
+	while (k != 2 * i - 1) {
+	   printf("* ");
+	   ++k;
+	}
+	printf("\n");
+ 
+}
+
+func main(){
+
+}
